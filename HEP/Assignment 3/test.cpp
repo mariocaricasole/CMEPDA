b3a37270395ca73d0f2e5f247b74d9af4523579e
mariocaricasole <caricasolemario@gmail.com>
2023-03-13 13:13:29 +0100

    added unit test for phonebook class, minor bug fixes

--- HEP/Assignment 3/test.cpp
+++ HEP/Assignment 3/test.cpp
@@ -11,21 +11,27 @@ int testPerson(Person person)
 	person.setName("Luigi");
 	std::cout << "New name is: " << person.name() <<std::endl;
 	person.print();
-	std::cout << "Test successful!";
+	std::cout << "Test successful!\n" << std::endl;
 	return 0;
 }
 
 int testPhoneBook(PhoneBook book, Person person)
 {
+	std::cout<< "Testing phonebook...\n";
 	book.addEntry(person);
 	book.print();
+	Person res = book.search(person.name()).second;
+	res.print();
+	res = book.search("Luigi Caricasole").second;
+	res.print();
+	std::cout << "Test successful" << std::endl;
 	return 0;
 }
 
 
 int main()
 {
-	Person mario("Mario", "3665803754","mariocarica@gmail.com");
+	Person mario("Mario Caricasole", "3665803754","mariocarica@gmail.com");
 	int res = testPerson(mario);
 	//test phonebook
 	PhoneBook book;
